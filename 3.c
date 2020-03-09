5ddeabffdef0c5bd07ce9aa2d372228580e692b1
Tejas7255 <61399259+Tejas7255@users.noreply.github.com>
2020-03-09 10:30:21 +0530

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,14 @@
+/*elseif*/
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter a,b,cvalues\n");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b&&a>c)
+printf("a\n");
+else if(b>a&&b>c)
+printf("b\n");
+else
+printf("c\n");
+}
---
+++ leap.c
@@ -0,0 +1,12 @@
+/*leap*/
+#include<stdio.h>
+void main()
+{
+int y;
+printf("enter the y value\n");
+scanf("%d",&y);
+if(y%4==0)
+printf("y is a leao year\n");
+else 
+printf("y is not a leap year\n");
+}
